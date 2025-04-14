841752dbb606b5758a1e501c3021cbedb8af43ac
MALIKA <140508883+Malika44@users.noreply.github.com>
2025-04-14 09:46:53 +0400

    Uploaded Soultion of Candle Problem

---
+++ PROBLEM SOLVING-D1.py
@@ -0,0 +1,14 @@
+candles = [4, 1, 4, 3]
+tallest = candles[0]  
+
+for height in candles:
+    if height > tallest:
+        tallest = height  
+
+count = 0
+for height in candles:
+    if height == tallest:
+        count += 1
+
+print(count)
+
